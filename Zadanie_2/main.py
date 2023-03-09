3c7912c0625fd25655ea1e4ce422e34f2af14848
Martin <xkochanm@stuba.sk>
2023-03-09 16:30:11 +0100

    Detection of the circles
    First version

--- Zadanie_2/main.py
+++ Zadanie_2/main.py
@@ -1,7 +1,8 @@
 
 import cv2 as cv
 import numpy as np
-from ximea import xiapi
+import sys
+# from ximea import xiapi
 
 def main():
     # print(cv.getBuildInformation())
@@ -11,7 +12,7 @@ def main():
     # define size of grid
     grid = np.zeros((5 * 7, 3), np.float32)
 
-    # create 2,7,6 array, transpose to 6,7,2 and reshape to 6*7,2
+    # create 2,7,6 array, transpose to 5,7,2 and reshape to 5*7,2
     grid[:, :2] = np.mgrid[0:7, 0:5].T.reshape(-1, 2)
 
     # create arrays to store data
@@ -43,10 +44,10 @@ def main():
     # # calibration
     # # in order: return value, camera matrix, distortion coefficients, rotation and translation vectors
     #
-    # ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-    # 
-    # print(mtx)
-    #
+    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+    print(mtx)
+
     # image = cv.imread("images2/img6.jpg")
     # h, w = image.shape[:2]
     # newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
@@ -60,6 +61,37 @@ def main():
 
         # cv.imshow("temp", image)
         # cv.waitKey()
+
+
+    # Detection of the circle
+    path2 = "images2/circle3.jpg"
+
+    # Loads an image
+    img = cv.imread(cv.samples.findFile(path2), cv.IMREAD_COLOR)
+
+    # Convert it to gray
+    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    # Reduce the noise to avoid false circle detection
+    img_gray = cv.medianBlur(img_gray, 5)
+
+    # Applying Hough Circle Transform
+    rows = img_gray.shape[0]
+    circle_coordinates = cv.HoughCircles(img_gray, cv.HOUGH_GRADIENT, 1, rows / 8, param1=100, param2=30, minRadius=70, maxRadius=150)
+
+    # Drawing detected circles
+    if circle_coordinates is not None:
+        circle_coordinates = np.uint16(np.around(circle_coordinates))
+        for i in circle_coordinates[0, :]:
+            center = (i[0], i[1])
+            # circle center
+            cv.circle(img, center, 1, (0, 100, 100), 3)
+            # circle outline
+            radius = i[2]
+            cv.circle(img, center, radius, (255, 0, 255), 3)
+
+    cv.imshow("detected circles", img)
+    cv.waitKey(0)
 
 if __name__ == '__main__':
     main()
