1b8d4a3474d74dc81443f78ad62e6fe1013055f4
Martin <xkochanm@stuba.sk>
2023-04-20 15:46:54 +0200

    3uloha hotovo

--- Zadanie_4/main.py
+++ Zadanie_4/main.py
@@ -6,14 +6,14 @@ import sys
 # sys.path.append('')
 # import open3d_tutorial as o3dtud
 
-def display_inlier_outlier(cloud, ind):
-    inlier_cloud = cloud.select_down_sample(ind)
-    outlier_cloud = cloud.select_down_sample(ind, invert=True)
-    # Showing outliers (red) and inliers (gray)
-    outlier_cloud.paint_uniform_color([1, 0, 0])
-    inlier_cloud.paint_uniform_color([0.8, 0.8, 0.8])
-    o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
-    return ()
+# def display_inlier_outlier(cloud, ind):
+#     inlier_cloud = cloud.select_down_sample(ind)
+#     outlier_cloud = cloud.select_down_sample(ind, invert=True)
+#     # Showing outliers (red) and inliers (gray)
+#     outlier_cloud.paint_uniform_color([1, 0, 0])
+#     inlier_cloud.paint_uniform_color([0.8, 0.8, 0.8])
+#     o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
+#     return ()
 
 def main():
     # dano location "C:\\Users\\Dano\\PycharmProjects\\Zadanie4\\my_pts.ply"
@@ -26,14 +26,14 @@ def main():
     # picture1 = o3d.io.read_point_cloud("C:\\Users\\Lenovo\\PycharmProjects\\PVSO_zad1\\Zadanie_4\\output_big2.pcd")
     # o3d.visualization.draw_geometries([picture1])
 
-    # # Downsample the point cloud with a voxel of 0.05
-    # # for my_pts.ply
-    # voxel_down_cloud_kinect = cloud_kinect.voxel_down_sample(voxel_size=0.05)
-    # o3d.visualization.draw_geometries([voxel_down_cloud_kinect])
-    #
-    # # for hmmPLY.ply
-    # voxel_down_cloud_downloaded = cloud_downloaded.voxel_down_sample(voxel_size=0.05)
-    # o3d.visualization.draw_geometries([voxel_down_cloud_downloaded])
+    # Downsample the point cloud with a voxel of 0.05
+    # for my_pts.ply
+    voxel_down_cloud_kinect = cloud_kinect.voxel_down_sample(voxel_size=0.03)
+    o3d.visualization.draw_geometries([voxel_down_cloud_kinect])
+
+    # for hmmPLY.ply
+    voxel_down_cloud_downloaded = cloud_downloaded.voxel_down_sample(voxel_size=0.03)
+    o3d.visualization.draw_geometries([voxel_down_cloud_downloaded])
     #
     #
     # # Every 15th points are selected
@@ -46,18 +46,16 @@ def main():
     # o3d.visualization.draw_geometries([uni_down_cloud_downloaded])
 
     # radius removal of points
-    # # for my_pts.ply
-    # cloud_kinect_removed, ind = cloud_kinect.remove_radius_outlier(nb_points=20, radius=0.05)
-    # # display_inlier_outlier(cloud_kinect, ind)
-    # o3d.visualization.draw_geometries([cloud_kinect_removed])
+    # for my_pts.ply
+    cloud_kinect_removed, ind = voxel_down_cloud_kinect.remove_radius_outlier(nb_points=5, radius=0.05)
+    # display_inlier_outlier(cloud_kinect, ind)
+    o3d.visualization.draw_geometries([cloud_kinect_removed])
 
     # for hmmPLY.ply
-    cloud_downloaded_removed, ind = cloud_downloaded.remove_radius_outlier(nb_points=30, radius=0.05)
+    cloud_downloaded_removed, ind = voxel_down_cloud_downloaded.remove_radius_outlier(nb_points=5, radius=0.05)
     # display_inlier_outlier(cloud_downloaded, ind)
     o3d.visualization.draw_geometries([cloud_downloaded_removed])
 
-
-    o3d.visualization.draw_geometries()
 
 
 # Press the green button in the gutter to run the script.
